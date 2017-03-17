5bb3bac3c6da57d9e3541f574e2ae796b43489c2
Behnam Najafi <behnam@hydra.(none)>
2017-03-17 00:28:24 +0000

    No running.

--- src/main.cpp
+++ src/main.cpp
@@ -37,48 +37,22 @@ int main(int argc, char * argv[]) {
 	if (inputs->anneal){
 		Anneal *anneal = new Anneal(inputs);
 		Local local_a = Local(constants, G, anneal, inputs);
-		local_a.run();
+		//local_a.run();
 	}
 	else if (inputs->melt){
 		Melt *melt = new Melt(inputs);
 		Local local_m = Local(constants, G_full, melt, inputs);
-		local_m.run();
-	}
-	else{
-		printf ("Error! Anneal_Melt not specified!\n"); exit (EXIT_FAILURE);
-	}
-/*
-	string file_params;
-	for (int i=1; i<argc; i++){
-		file_params.append("_");
-		file_params.append(argv[i]);
-	}
-
-	MyGraph *G = new MyGraph(tile);	
-	Anneal *anneal = new Anneal(100.,40.,Cpm2spC(arguments[4]),1);
-	Local local_a = Local(constants, G, anneal);
-
-	Melt *melt = new Melt(40.,100.,Cpm2spC(arguments[4]),1);
-	MyGraph *G_full = new MyGraph(Tile(origami));
-	for (int i=0; i<tile.n_domains; i++){G_full->make_transition(i,true);}
-	G_full->reset_all_states();
-	Local local_m = Local(constants, G_full, melt);
-
-	if (Anneal_Melt == "Anneal"){
-		local_a.run("Anneal_"+HUS+file_params, seed);
-	}
-	else if (Anneal_Melt == "Melt"){
-		local_m.run("Melt_"+HUS+file_params, seed);
+		//local_m.run();
 	}
 	else{
 		printf ("Error! Anneal_Melt not specified!\n"); exit (EXIT_FAILURE);
 	}
 
+	delete inputs;
 	delete constants;
 	delete G;
-	delete anneal;
-	delete melt;
-*/
+	//delete anneal;
+	//delete melt;
 
 	return 0;
 }
